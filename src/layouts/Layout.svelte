3d1271b63192682b1e47aa287f20c9b20f6018d8
spock-mark1 <spockwoz@gmail.com>
2023-09-13 15:12:57 +0900

    deployment

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,7 +1,145 @@
 <script>
+  import { onMount } from 'svelte';
+
+  const darkModeClass = 'dark';
+
+  let isDarkMode;
+  onMount(() => {
+    isDarkMode = window.document.documentElement.classList.contains(darkModeClass);
+  });
+
+  function changeTheme() {
+    window.document.documentElement.classList.toggle(darkModeClass);
+    isDarkMode = window.document.documentElement.classList.contains(darkModeClass);
+    window.localStorage.setItem('colour-mode', isDarkMode ? 'dark' : 'light');
+  }
+
   export let templateHtml;
 </script>
 
-<div class="container">
-  {@html templateHtml}
+<svelte:head>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      darkMode: 'class',
+      // plugins: [typographyPlugin],
+      theme: {
+        fontSize: {
+          xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+          sm: ['0.875rem', { lineHeight: '1.5rem' }],
+          base: ['1rem', { lineHeight: '1.75rem' }],
+          lg: ['1.125rem', { lineHeight: '1.75rem' }],
+          xl: ['1.25rem', { lineHeight: '2rem' }],
+          '2xl': ['1.5rem', { lineHeight: '2rem' }],
+          '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+          '4xl': ['2rem', { lineHeight: '2.5rem' }],
+          '5xl': ['3rem', { lineHeight: '3.5rem' }],
+          '6xl': ['3.75rem', { lineHeight: '1' }],
+          '7xl': ['4.5rem', { lineHeight: '1' }],
+          '8xl': ['6rem', { lineHeight: '1' }],
+          '9xl': ['8rem', { lineHeight: '1' }],
+        },
+        // typography: typographyStyles,
+      },
+    };
+  </script>
+</svelte:head>
+
+
+<div class="flex h-full bg-zinc-50 dark:bg-black">
+  <div class="flex w-full">
+    <div class="fixed inset-0 flex justify-center sm:px-8">
+      <div class="flex w-full max-w-7xl lg:px-8">
+        <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20"></div>
+      </div>
+    </div>
+    <div class="relative flex w-full flex-col">
+      <header class="pointer-events-none relative z-50 flex flex-none flex-col" style="height:var(--header-height);margin-bottom:var(--header-mb)">
+        <div class="top-0 z-10 h-16 pt-6" style="position:var(--header-position)">
+          <div class="sm:px-8 top-[var(--header-top,theme(spacing.6))] w-full" style="position:var(--header-inner-position)">
+            <div class="mx-auto w-full max-w-7xl lg:px-8">
+              <div class="relative px-4 sm:px-8 lg:px-12">
+                <div class="mx-auto max-w-2xl lg:max-w-5xl">
+                  <div class="relative flex gap-4">
+                    <div class="flex flex-1">
+                      Blog Title
+                    </div>
+                    <div class="flex flex-1 justify-end md:justify-center">
+                      <div class="pointer-events-auto md:hidden" data-headlessui-state="">
+                        <button class="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20" type="button" aria-expanded="false" data-headlessui-state="" id="headlessui-popover-button-:R2miqla:">
+                          Menu
+                          <svg viewBox="0 0 8 6" aria-hidden="true" class="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400">
+                            <path d="M1.75 1.75 4 4.25l2.25-2.5" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                          </svg>
+                        </button>
+                      </div>
+                      <div style="position:fixed;top:1px;left:1px;width:1px;height:0;padding:0;margin:-1px;overflow:hidden;clip:rect(0, 0, 0, 0);white-space:nowrap;border-width:0;display:none"></div>
+                      <nav class="pointer-events-auto hidden md:block">
+                        <ul class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                          <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="/about">About</a></li>
+                          <li><a class="relative block px-3 py-2 transition text-teal-500 dark:text-teal-400" href="/articles">Articles<span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span></a></li>
+                          <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="/projects">Projects</a></li>
+                          <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="/speaking">Speaking</a></li>
+                          <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="/uses">Uses</a></li>
+                        </ul>
+                      </nav>
+                    </div>
+                    <div class="flex justify-end md:flex-1">
+<!--                      <div class="pointer-events-auto">-->
+<!--                        <button type="button" aria-label="Switch to dark theme"-->
+<!--                                class="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"-->
+<!--                                on:click={changeTheme}-->
+<!--                        >-->
+<!--                          <svg viewBox="0 0 24 24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600">-->
+<!--                            <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z"></path>-->
+<!--                            <path d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061" fill="none"></path>-->
+<!--                          </svg>-->
+<!--                          <svg viewBox="0 0 24 24" aria-hidden="true" class="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500">-->
+<!--                            <path d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>-->
+<!--                          </svg>-->
+<!--                        </button>-->
+<!--                      </div>-->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main class="flex-auto">
+        <div class="sm:px-8 mt-16 sm:mt-32">
+          <div class="mx-auto w-full max-w-7xl lg:px-8">
+            <div class="relative px-4 sm:px-8 lg:px-12">
+              <div class="mx-auto max-w-2xl lg:max-w-5xl">
+
+                {@html templateHtml}
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer class="mt-32 flex-none">
+        <div class="sm:px-8">
+          <div class="mx-auto w-full max-w-7xl lg:px-8">
+            <div class="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+              <div class="relative px-4 sm:px-8 lg:px-12">
+                <div class="mx-auto max-w-2xl lg:max-w-5xl">
+                  <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                    <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200"><a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/about">About</a><a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/projects">Projects</a><a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/speaking">Speaking</a><a class="transition hover:text-teal-500 dark:hover:text-teal-400" href="/uses">Uses</a></div>
+                    <p class="text-sm text-zinc-400 dark:text-zinc-500">
+                      © <!-- -->2023<!-- --> Spencer Sharp. All rights reserved.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
 </div>
--- src/routes/blog/Blog.svelte
+++ src/routes/blog/Blog.svelte
@@ -3,63 +3,33 @@
   const { html, frontmatter } = data;
 </script>
 
-<style>
-  h1 {
-    margin-bottom: 10px;
-  }
-
-  .title {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    padding-bottom: 1rem;
-    border-bottom: 1px solid #ddd;
-  }
-
-  :global(h2) {
-    margin-top: 2rem;
-  }
-
-  :global(pre) {
-    background: #eee;
-    padding: 1rem;
-    border-radius: 1rem;
-  }
-
-  :global(blockquote) {
-    margin: 0;
-    background: #ddd;
-    padding: 3px 1.5rem 3px 3rem;
-    position: relative;
-    border-radius: 1rem;
-  }
-  :global(blockquote:after) {
-    content: '>';
-    color: #aaa;
-    font-size: 30px;
-    position: absolute;
-    top: 33%;
-    left: 1rem;
-  }
-
-  :global(blockquote p) {
-    padding: 0;
-  }
-</style>
-
 <svelte:head>
   <title>{frontmatter.title}</title>
   <meta name="description" content={frontmatter.excerpt} />
   <link href="{settings.origin}{request.permalink}" rel="canonical" />
 </svelte:head>
-<a href="/">&LeftArrow; Home</a>
 
-<div class="title">
-  <h1>{frontmatter.title}</h1>
-  {#if frontmatter.author}<small>By {frontmatter.author}</small>{/if}
+<div class="xl:relative">
+  <div class="mx-auto max-w-2xl">
+    <button type="button" aria-label="Go back to articles" class="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0">
+      <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400">
+        <path d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      </svg>
+    </button>
+    <article>
+      <header class="flex flex-col">
+        <h1 class="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          {frontmatter.title}
+        </h1>
+        <time datetime="2022-09-05" class="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"><span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span><span class="ml-3">September 5, 2022</span></time>
+      </header>
+      <div class="mt-8 prose dark:prose-invert" data-mdx-content="true">
+        {#if html}
+          {@html html}
+        {:else}
+          <h1>Oops!! Markdown not found!</h1>
+        {/if}
+      </div>
+    </article>
+  </div>
 </div>
-
-{#if html}
-  {@html html}
-{:else}
-  <h1>Oops!! Markdown not found!</h1>
-{/if}
