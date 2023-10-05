a8ffdc068ef6d0318726ae3403568511a94eca15
spock-mark1 <spockwoz@gmail.com>
2023-10-05 19:59:33 +0900

    deployment

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -17,33 +17,39 @@
   export let templateHtml;
 </script>
 
-<svelte:head>
-<!--  <script src="https://cdn.tailwindcss.com"></script>-->
-  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-  <script>
-    tailwind.config = {
-      darkMode: 'class',
-      theme: {
-        fontSize: {
-          xs: ['0.8125rem', { lineHeight: '1.5rem' }],
-          sm: ['0.875rem', { lineHeight: '1.5rem' }],
-          base: ['1rem', { lineHeight: '1.75rem' }],
-          lg: ['1.125rem', { lineHeight: '1.75rem' }],
-          xl: ['1.25rem', { lineHeight: '2rem' }],
-          '2xl': ['1.5rem', { lineHeight: '2rem' }],
-          '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-          '4xl': ['2rem', { lineHeight: '2.5rem' }],
-          '5xl': ['3rem', { lineHeight: '3.5rem' }],
-          '6xl': ['3.75rem', { lineHeight: '1' }],
-          '7xl': ['4.5rem', { lineHeight: '1' }],
-          '8xl': ['6rem', { lineHeight: '1' }],
-          '9xl': ['8rem', { lineHeight: '1' }],
-        },
-      },
-    };
-  </script>
-</svelte:head>
+<!--<svelte:head>-->
+<!--  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>-->
+<!--  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CPPXMY0TJD"></script>-->
 
+<!--  <script>-->
+<!--    tailwind.config = {-->
+<!--      darkMode: 'class',-->
+<!--      theme: {-->
+<!--        fontSize: {-->
+<!--          xs: ['0.8125rem', { lineHeight: '1.5rem' }],-->
+<!--          sm: ['0.875rem', { lineHeight: '1.5rem' }],-->
+<!--          base: ['1rem', { lineHeight: '1.75rem' }],-->
+<!--          lg: ['1.125rem', { lineHeight: '1.75rem' }],-->
+<!--          xl: ['1.25rem', { lineHeight: '2rem' }],-->
+<!--          '2xl': ['1.5rem', { lineHeight: '2rem' }],-->
+<!--          '3xl': ['1.875rem', { lineHeight: '2.25rem' }],-->
+<!--          '4xl': ['2rem', { lineHeight: '2.5rem' }],-->
+<!--          '5xl': ['3rem', { lineHeight: '3.5rem' }],-->
+<!--          '6xl': ['3.75rem', { lineHeight: '1' }],-->
+<!--          '7xl': ['4.5rem', { lineHeight: '1' }],-->
+<!--          '8xl': ['6rem', { lineHeight: '1' }],-->
+<!--          '9xl': ['8rem', { lineHeight: '1' }],-->
+<!--        },-->
+<!--      },-->
+<!--    };-->
+<!--  </script>-->
+<!--  <script>-->
+<!--    window.dataLayer = window.dataLayer || [];-->
+<!--    function gtag(){dataLayer.push(arguments);}-->
+<!--    gtag('js', new Date());-->
+<!--    gtag('config', 'G-CPPXMY0TJD');-->
+<!--  </script>-->
+<!--</svelte:head>-->
 
 <!--<div class="flex h-full bg-zinc-50 dark:bg-black">-->
 <div class="flex h-full bg-zinc-50">
@@ -142,4 +148,39 @@
       </footer>
     </div>
   </div>
+</div>
+
+
+<div>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                fontSize: {
+                    xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+                    sm: ['0.875rem', { lineHeight: '1.5rem' }],
+                    base: ['1rem', { lineHeight: '1.75rem' }],
+                    lg: ['1.125rem', { lineHeight: '1.75rem' }],
+                    xl: ['1.25rem', { lineHeight: '2rem' }],
+                    '2xl': ['1.5rem', { lineHeight: '2rem' }],
+                    '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+                    '4xl': ['2rem', { lineHeight: '2.5rem' }],
+                    '5xl': ['3rem', { lineHeight: '3.5rem' }],
+                    '6xl': ['3.75rem', { lineHeight: '1' }],
+                    '7xl': ['4.5rem', { lineHeight: '1' }],
+                    '8xl': ['6rem', { lineHeight: '1' }],
+                    '9xl': ['8rem', { lineHeight: '1' }],
+                },
+            },
+        };
+    </script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CPPXMY0TJD"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-CPPXMY0TJD');
+    </script>
 </div>
